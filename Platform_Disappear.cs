3d27a32aee4b5f47ae91295320f12713211aa100
Loránd Kele <lorand996@gmail.com>
2023-01-16 09:18:23 +0100

    Eltuno padlo

---
+++ Platform_Disappear.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Platform_Disappear : Node2D
+{
+
+
+    public override void _Ready()
+    {
+
+    }
+
+
+    public override void _Process(float delta)
+    {
+
+    }
+
+    public void _on_Area2D_body_entered(KinematicBody2D player)
+    {
+        QueueFree();
+    }
+}
