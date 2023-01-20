24e2a36fc5c197756d77083646f2bddffa5493a6
Loránd Kele <lorand996@gmail.com>
2023-01-20 09:12:02 +0100

    asd

--- KinematicBody2D1.cs
+++ KinematicBody2D1.cs
@@ -3,145 +3,15 @@ using System;
 
 public class KinematicBody2D1 : KinematicBody2D
 {
-	float gravitacio = 10;
-	float sebesseg = 200;
+	float gravitacio = 8;
+	int hp = 3;
+	float sebesseg = 400;
 	bool wasonwall = false;
 	Vector2 mozgas = Vector2.Zero;
 	KinematicCollision2D m_utkozes;
 	public override void _PhysicsProcess(float delta)
 	{
-		//base._PhysicsProcess(delta);
-		//MoveAndCollide(new Vector2(0, 200 * delta));
-		//MoveAndCollide(new Vector2(200 * delta, 200 * delta)); //Ütközés után megáll.
-		//MoveAndSlide(new Vector2(200, 200)); //Ütközés után csúszik. (A deltával számol.)
-		/*
-		//Gravitáció alkalmazása
-		mozgas.y += gravitacio * delta;
-		MoveAndCollide(mozgas);
-		GD.Print(mozgas.y);
-		// Nem jó mert a padlón nő a gravitációs vektor. (Egyre lejjebb szeretné tenni.)
-		*/
-		/*
-		mozgas.y += gravitacio * delta;
-		KinematicCollision2D utkozes = MoveAndCollide(mozgas); 
-		GD.Print(mozgas.y);
-		if (utkozes != null) mozgas.y = 0;
-		//Ez már jobb de nincs sebessége
-		*/
-		/*
-		mozgas.y += gravitacio * delta;
-		mozgas.x = sebesseg * delta;
-		KinematicCollision2D utkozes = MoveAndCollide(mozgas); 
-		GD.Print(mozgas.y);
-		if (utkozes != null) mozgas.y = 0;
-		//Nem jó mert mindig változik a mozgásvektor és emiatt elmozdul.
-		*/
-		/*
-		if (m_utkozes == null)
-		{
-			mozgas.y += gravitacio * delta;
-			mozgas.x = sebesseg * delta;
-		}
-		m_utkozes = MoveAndCollide(mozgas);
-		GD.Print(mozgas.y);
-		Ez már jó egy nem mozgatható testre
-		*/
-		/*
-		mozgas.y += gravitacio * delta;
-		if (Input.IsActionPressed("ui_left"))
-		{
-			mozgas.x = -sebesseg * delta;
-		}
-		if (Input.IsActionPressed("ui_right"))
-		{
-			mozgas.x = sebesseg * delta;
-		}
-		KinematicCollision2D utkozes = MoveAndCollide(mozgas); 
-		GD.Print(mozgas.y);
-		if (utkozes != null) mozgas.y = 0;
-		//Nehezen akadozva mozog ezért mozgatható testnél nem használjuk a mc-t
-		*/
-		/*
-		mozgas.y += gravitacio;
-		if (Input.IsActionPressed("ui_left"))
-		{
-			mozgas.x = -sebesseg;
-		}
-		if (Input.IsActionPressed("ui_right"))
-		{
-			mozgas.x = sebesseg;
-		}
-		MoveAndSlide(mozgas);
-		GD.Print(mozgas.y);
-		//Itt sem jó a gravitáció kezelés
-		*/
-		/*
-		if (! IsOnFloor()) mozgas.y += gravitacio;
-		else mozgas.y = gravitacio;
-		if (Input.IsActionPressed("ui_left"))
-		{
-			mozgas.x = -sebesseg;
-		}
-		if (Input.IsActionPressed("ui_right"))
-		{
-			mozgas.x = sebesseg;
-		}
-		MoveAndSlide(mozgas, Vector2.Up);
-		GD.Print(mozgas.y);
-		//Ez már jó csak nem tudom megállítani és nem ugrik.
-		*/
-		/*
-		if (! IsOnFloor()) mozgas.y += gravitacio;
-		else mozgas.y = gravitacio;
-		if (Input.IsActionPressed("ui_left"))
-		{
-			mozgas.x = -sebesseg;
-		}
-		if (Input.IsActionPressed("ui_right"))
-		{
-			mozgas.x = sebesseg;
-		}
-		if (Input.IsActionPressed("ui_down"))
-		{
-			mozgas.x = 0;
-		}
-		if (Input.IsActionJustPressed("ui_up"))
-		{
-			mozgas.y = -300;
-		}
-		MoveAndSlide(mozgas, Vector2.Up);
-		//Nem jó mert a levegőben is ugorhat.
-		*/
-		/*
-		if (IsOnFloor()) GD.Print($"Padlón");
-		if (IsOnCeiling()) GD.Print($"Plafonon");
-		if (IsOnWall()) GD.Print($"Falon");
-		if (! IsOnFloor() && ! IsOnCeiling() && ! IsOnWall()) GD.Print($"Levegőben");
-		if (! IsOnFloor()) mozgas.y += gravitacio;
-		else mozgas.y = gravitacio;
-		if (Input.IsActionPressed("ui_left"))
-		{
-			mozgas.x = -sebesseg;
-		}
-		if (Input.IsActionPressed("ui_right"))
-		{
-			mozgas.x = sebesseg;
-		}
-		if (Input.IsActionPressed("ui_down"))
-		{
-			mozgas.x = 0;
-		}
-		if (Input.IsActionJustPressed("ui_up"))
-		{
-			if (IsOnFloor() || IsOnWall()) mozgas.y = -300; //Ha a falról is ugorhat
-		}
-		MoveAndSlide(mozgas, Vector2.Up);
-		//Nem jó mert a lejtőn a levegőben lesz és onnan nem ugorhat.
-		*/
-		//if (IsOnFloor()) GD.Print($"Padlón");
-		//if (IsOnCeiling()) GD.Print($"Plafonon");
-		//if (IsOnWall()) GD.Print($"Falon");
-		//if (! IsOnFloor() && ! IsOnCeiling() && ! IsOnWall()) GD.Print($"Levegőben");
+	
 		if (! IsOnFloor()) mozgas.y += gravitacio;
 		else mozgas.y = gravitacio;
 		if (Input.IsActionPressed("ui_left"))
@@ -164,7 +34,7 @@ public class KinematicBody2D1 : KinematicBody2D
 		if (Input.IsActionJustPressed("ui_up"))
 		{   if(IsOnFloor() && IsOnWall()) wasonwall = false;
 			if (IsOnFloor()){
-				 mozgas.y = -300; //Ha a falról is ugorhat
+				 mozgas.y = -300; 
 				 wasonwall = false;
 			}
 			
@@ -177,5 +47,13 @@ public class KinematicBody2D1 : KinematicBody2D
 			
 		}
 		else MoveAndSlideWithSnap(mozgas, new Vector2(0, 6), Vector2.Up);
+
+		if (hp == 0)
+		{
+			sebesseg = 0;
+			mozgas.y = -250;
+			gravitacio = 0;
+			mozgas.x = 0;
+		}
 	}
 }
