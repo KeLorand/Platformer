22f2bf5aa4f3a308b0407f314796b66601ffb3ba
Loránd Kele <lorand996@gmail.com>
2023-02-02 08:49:37 +0100

    s/ma

--- Vilag.cs
+++ Vilag.cs
@@ -6,13 +6,11 @@ public class Vilag : Node2D
     Camera2D Kamera;
     Node2D Jatekos;
     Node2D Ellenseg;
-    Node2D DeathScreen;
-    Vector2 RespawnPoint = new Vector2(0, 0);
 
 
     public override void _Process(float delta)
     {
-        Kamera.Position = new Vector2(Jatekos.Position.x, Jatekos.Position.y + 300);
+        Kamera.Position = new Vector2(Jatekos.Position.x, Kamera.Position.y);
     }
     public override void _Ready()
     {
@@ -23,6 +21,6 @@ public class Vilag : Node2D
     private void _on_Area2D_body_entered(object area)
     {
         GD.Print("hello");
-        Jatekos.Position = RespawnPoint;
+        GetTree().ChangeScene("res://DeathScreen.tscn");
     }
 }
