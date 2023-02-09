f15762675c7cef3522b22c1c5d78ada5d6f0af04
Loránd Kele <lorand996@gmail.com>
2023-02-09 09:08:30 +0100

    FASZ

--- ProjectileShooter.cs
+++ ProjectileShooter.cs
@@ -1,38 +1,41 @@
 using Godot;
 
 public class ProjectileShooter : Node
-{   
+{
     float timer;
     bool fire;
+
+    [Export]public PackedScene Projectile;
     private void ShootProjectile()
     {
-        var projectile = new RigidBody2D();
-        AddChild(projectile);
 
-        var sprite = new Sprite();
-        sprite.Texture = (Texture)ResourceLoader.Load("res://Képernyőkép 2023-02-06 085450.png");
-        projectile.AddChild(sprite);
-
-        projectile.SetLinearVelocity(new Vector2(-1000, 0));
+        var ProjectileRB = new RigidBody2D();
+        Node2D projectile = (Node2D)Projectile.Instance();
+        projectile.AddChild(ProjectileRB);
+        this.AddChild(projectile);
+        ProjectileRB.SetLinearVelocity(new Vector2(-1000, 0));
     }
 
     public override void _Process(float delta)
-    {   
+    {
         timer += delta;
-        if(timer >2 && fire == true){
-                ShootProjectile();
-                timer = 0;
+        if (timer > 2 && fire == true)
+        {
+            ShootProjectile();
+            timer = 0;
         }
-            
+
 
     }
-    public void _on_Area2D_body_entered(KinematicBody2D Jatekos){
-            
-            fire = true;
-            
+    public void _on_Area2D_body_entered(KinematicBody2D Jatekos)
+    {
+
+        fire = true;
+
 
     }
-    public void _on_Area2D_body_exited(KinematicBody2D Jatekos){
+    public void _on_Area2D_body_exited(KinematicBody2D Jatekos)
+    {
         fire = false;
     }
 }
