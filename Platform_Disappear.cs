87bef0aeaf3ddc03411fa319ab44eb8a6be4179b
Kancsal Máté <71323998+ahelyikemeny@users.noreply.github.com>
2023-02-10 09:20:40 +0100

    as

--- Platform_Disappear.cs
+++ Platform_Disappear.cs
@@ -1,19 +1,18 @@
 using Godot;
 using System;
 
+
 public class Platform_Disappear : Node2D
 {
-
-
+    [Export]public PackedScene Platform;
+    float timer;
     public override void _Ready()
     {
-
     }
-
 
     public override void _Process(float delta)
     {
-
+        
     }
 
     public void _on_Area2D_body_entered(KinematicBody2D player)
