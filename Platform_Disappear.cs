4bbd4e22708c30a62f2c911b39e28c9dc326c845
Kancsal Máté <71323998+ahelyikemeny@users.noreply.github.com>
2023-02-12 19:32:44 +0100

    menu talan

--- Platform_Disappear.cs
+++ Platform_Disappear.cs
@@ -4,30 +4,30 @@ using System;
 
 public class Platform_Disappear : Node2D
 {
-    [Export]public PackedScene Platform;
-    float timer;
-    bool vane;
-    private Sprite sprite;
-    public override void _Ready()
-    {
-        sprite = GetNode<Sprite>("Sprite");
-        sprite.Modulate = new Color(165, 23, 0, 255);
-    }
+	[Export]public PackedScene Platform;
+	float timer;
+	bool vane;
+	private Sprite sprite;
+	public override void _Ready()
+	{
+		sprite = GetNode<Sprite>("Sprite");
+		sprite.Modulate = new Color(165, 23, 0, 255);
+	}
 
-    public override void _Process(float delta)
-    {   
-        timer += delta;
-        if (timer >= 10)
-        {
-            sprite.Modulate = new Color(0, 0, 0, 255);
-            vane = false;
-        }
-    }
+	public override void _Process(float delta)
+	{   
+		timer += delta;
+		if (timer >= 10)
+		{
+			sprite.Modulate = new Color(0, 0, 0, 255);
+			vane = false;
+		}
+	}
 
-    public void _on_Area2D_body_entered(KinematicBody2D player)
-    {
-        sprite.Modulate =new Color(0,0,0,0);
-         vane = true;
-         timer = 0;
-    }
+	public void _on_Area2D_body_entered(KinematicBody2D player)
+	{
+		sprite.Modulate =new Color(0,0,0,0);
+		 vane = true;
+		 timer = 0;
+	}
 }
