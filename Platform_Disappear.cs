b6f890d1959d1f966574c4497116524b5bd84fa2
Loránd Kele <lorand996@gmail.com>
2023-02-14 13:03:02 +0100

    asd

--- Platform_Disappear.cs
+++ Platform_Disappear.cs
@@ -9,19 +9,23 @@ public class Platform_Disappear : Node2D
     }
 
     public override void _Process(float delta)
-    {   
+    {
 
     }
 
     public void _on_Area2D_body_entered(KinematicBody2D player)
     {
-        this.Visible = false;
-        Timer timer = new Timer();
-        timer.WaitTime = 10;
-        timer.OneShot = true;
-        timer.Connect("timeout", this, nameof(MakeVisible));
-        AddChild(timer);
-        timer.Start();
+        if (player.Name == "JatekosBody")
+        {
+            this.Visible = false;
+            Timer timer = new Timer();
+            timer.WaitTime = 10;
+            timer.OneShot = true;
+            timer.Connect("timeout", this, nameof(MakeVisible));
+            AddChild(timer);
+            timer.Start();
+        }
+
     }
 
     private void MakeVisible()
